ac456be35ec1191128d66a1c75b74312da1b8126
Kaidstor <kaidstor@gmail.com>
2024-04-24 19:38:59 +0500

    working version

--- index.ts
+++ index.ts
@@ -1,1 +1,25 @@
-export const add = (a: number, b: number) => a + b;
+export const sendNotification = (url: string, payload: IMsgPayload) => {
+
+   payload = {
+      ...payload,
+      type: payload.type || "warning",
+   }
+
+  return fetch(url + "/api/send/", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(payload),
+  });
+};
+
+export type IMsgType = "info" | "warning" | "unhandled" | "fatal";
+
+export type IMsgPayload = {
+  password: string;
+  text: string;
+  type?: IMsgType;
+  description?: string;
+  threadId: number;
+};
